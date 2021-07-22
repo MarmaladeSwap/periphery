9975509496d1f7644842ae9ea0d077d9919ef570
MarmaladeSwap <84860203+MarmaladeSwap@users.noreply.github.com>
2021-07-22 06:50:53 +0300

    Add files via upload

--- interfaces/IMrmFactory.sol
+++ interfaces/IMrmFactory.sol
@@ -13,5 +13,6 @@ interface IMrmFactory {
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
     function setFeeTo(address) external;
-    function setFeeToSetter(address) external;
+    function setFeeToSetter(address) external; 
+	
 }
